d1f380d749402635b24148ec245a150533e8745b
luislinodev <luislinodev@gmail.com>
2026-07-18 14:28:12 -0500

    Cambios settings

--- z_app_fle_pruebas/settings.py
+++ z_app_fle_pruebas/settings.py
@@ -156,9 +156,9 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
